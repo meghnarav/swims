209294b51b64c1de06ea5be663ad5daa81c7f793
meghna <meghnaravikumar06@gmail.com>
2026-03-05 20:49:40 +0530

    fix: localhost - 127.0.0.1

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='your_password',
-        database='swims'
+        host="127.0.0.1",
+        port=3306,
+        user="swims_user",
+        password="swims_pass",
+        database="swims"
     )
