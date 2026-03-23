a3d5cde496cbc42dab3a3225dd149fd502f866fc
meghna <meghnaravikumar06@gmail.com>
2026-03-23 23:03:34 +0530

    fix: api connection

---
+++ ui/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',
+      '/suppliers': 'http://127.0.0.1:8000',
+      '/products': 'http://127.0.0.1:8000',
+      '/warehouses': 'http://127.0.0.1:8000',
+      '/employees': 'http://127.0.0.1:8000',
+      '/inventory': 'http://127.0.0.1:8000',
+      '/transactions': 'http://127.0.0.1:8000',
+      '/categories': 'http://127.0.0.1:8000',
+      '/roles': 'http://127.0.0.1:8000',
+      '/stock-movements': 'http://127.0.0.1:8000',
+      '/products-detailed': 'http://127.0.0.1:8000',
+      '/employees-detailed': 'http://127.0.0.1:8000',
+      '/health': 'http://127.0.0.1:8000',
+    }
+  }
+})
