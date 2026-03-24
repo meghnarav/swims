1ea89b2af7e5b64075631bcd4f10508f839c6d74
meghna <meghnaravikumar06@gmail.com>
2026-03-24 21:30:47 +0530

    fix: punctuation

--- backend/queries.py
+++ backend/queries.py
@@ -433,8 +433,6 @@ def delete_employee(employee_id):
     conn = get_connection()
     cursor = conn.cursor()
     try:
-        cursor.execute("DELETE FROM Permanent_Employee WHERE employee_id=%s", (employee_id,))
-        cursor.execute("DELETE FROM Contract_Employee WHERE employee_id=%s", (employee_id,))
         cursor.execute("DELETE FROM Employee WHERE employee_id=%s", (employee_id,))
         conn.commit()
         return cursor.rowcount > 0
